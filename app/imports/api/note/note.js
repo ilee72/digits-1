821cd1ff86ae7e1cd243f3d1219b9080a357d36d
Jason Maligon <jason.maligon@gmail.com>
2018-03-20 22:52:19 -1000

    notes-2 finished in 18 minutes

--- app/imports/api/note/note.js
+++ app/imports/api/note/note.js
@@ -3,22 +3,18 @@ import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
 /** Create a Meteor collection. */
-const Stuffs = new Mongo.Collection('Stuffs');
+const Notes = new Mongo.Collection('Notes');
 
 /** Create a schema to constrain the structure of documents associated with this collection. */
-const StuffSchema = new SimpleSchema({
-  name: String,
-  quantity: Number,
+const NoteSchema = new SimpleSchema({
+  note: String,
+  contactId: String,
+  createdAt: Date,
   owner: String,
-  condition: {
-    type: String,
-    allowedValues: ['excellent', 'good', 'fair', 'poor'],
-    defaultValue: 'good',
-  },
 }, { tracker: Tracker });
 
 /** Attach this schema to the collection. */
-Stuffs.attachSchema(StuffSchema);
+Notes.attachSchema(NoteSchema);
 
 /** Make the collection and schema available to other code. */
-export { Stuffs, StuffSchema };
+export { Notes, NoteSchema };
